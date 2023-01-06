49a4b96292f53c156a33f63fdc52e6efe3247aeb
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-01-06 23:44:10 +0530

    add: heading

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Container = () => {
-  return <div>Container</div>;
+  return (
+    <div>
+      <h2>DISCOVER PODCASTS</h2>
+    </div>
+  );
 };
 
 export default Container;
