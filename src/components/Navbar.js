25abe9568e7f9a7b5cb649e1d3c40a86e1e5c98e
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-01-06 23:43:58 +0530

    add: search and profile section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./styles/Navbar.css";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__search">
+        <input
+          placeholder="Search something..."
+          className="navbar__search-input"
+        />
+        <i className="fa-solid fa-magnifying-glass navbar__search-magnifying-glass"></i>
+      </div>
+      <div>
+        <span>
+          <i className="fa-regular fa-heart"></i>
+        </span>
+        <span>
+          {" "}
+          <i className="fa-regular fa-bell"></i>
+        </span>
+        <div>Profile</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
