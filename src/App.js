44a315d4a23b60b25e77e28e089878c465110110
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-01-06 23:01:25 +0530

    add: Menubar and Home components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import "./App.css";
+import Menubar from "./components/Menubar";
+import Home from "./pages/Home";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Menubar />
+      <Home />
+    </div>
+  );
 };
 
 export default App;
