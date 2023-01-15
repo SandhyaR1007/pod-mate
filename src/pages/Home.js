aa18570a8c4b8d738e920878b1cbbb97a63d8f86
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-01-15 20:56:58 +0530

    add: Heading component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./styles/Home.css";
-import Navbar from "../components/Navbar";
-import Container from "../components/Container";
+
+import Container from "../components/container/Container";
+
+import Heading from "../components/navbar/Heading";
+import Navbar from "../components/navbar/Navbar";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Heading />
       <Container />
     </div>
   );
